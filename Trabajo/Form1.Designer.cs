94134c5df67502e586f3b6ab9a4d14ec04518f44
OmarCR117 <omarcabrera.r117@gmail.com>
2023-03-23 11:55:40 -0400

    Se agrega el Proyecto

---
+++ Trabajo/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Trabajo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.Fondo = new System.Windows.Forms.Panel();
+            this.btn_Agregar = new System.Windows.Forms.Button();
+            this.Datos = new System.Windows.Forms.DataGridView();
+            this.PlazoI = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PlazoF = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tasa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txt_Tasa = new System.Windows.Forms.TextBox();
+            this.txt_PlazoF = new System.Windows.Forms.TextBox();
+            this.txt_PlazoI = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ErrorP = new System.Windows.Forms.ErrorProvider(this.components);
+            this.Fondo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Datos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Fondo
+            // 
+            this.Fondo.Controls.Add(this.btn_Agregar);
+            this.Fondo.Controls.Add(this.Datos);
+            this.Fondo.Controls.Add(this.txt_Tasa);
+            this.Fondo.Controls.Add(this.txt_PlazoF);
+            this.Fondo.Controls.Add(this.txt_PlazoI);
+            this.Fondo.Controls.Add(this.label3);
+            this.Fondo.Controls.Add(this.label2);
+            this.Fondo.Controls.Add(this.label1);
+            this.Fondo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Fondo.Location = new System.Drawing.Point(0, 0);
+            this.Fondo.Name = "Fondo";
+            this.Fondo.Size = new System.Drawing.Size(800, 450);
+            this.Fondo.TabIndex = 0;
+            // 
+            // btn_Agregar
+            // 
+            this.btn_Agregar.Location = new System.Drawing.Point(536, 39);
+            this.btn_Agregar.Name = "btn_Agregar";
+            this.btn_Agregar.Size = new System.Drawing.Size(75, 41);
+            this.btn_Agregar.TabIndex = 7;
+            this.btn_Agregar.Text = "Agregar";
+            this.btn_Agregar.UseVisualStyleBackColor = true;
+            this.btn_Agregar.Click += new System.EventHandler(this.btn_Agregar_Click);
+            // 
+            // Datos
+            // 
+            this.Datos.AllowUserToAddRows = false;
+            this.Datos.AllowUserToDeleteRows = false;
+            this.Datos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Datos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.PlazoI,
+            this.PlazoF,
+            this.Tasa});
+            this.Datos.Location = new System.Drawing.Point(73, 104);
+            this.Datos.Name = "Datos";
+            this.Datos.ReadOnly = true;
+            this.Datos.Size = new System.Drawing.Size(427, 280);
+            this.Datos.TabIndex = 6;
+            // 
+            // PlazoI
+            // 
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlazoI.DefaultCellStyle = dataGridViewCellStyle2;
+            this.PlazoI.HeaderText = "Plazo de Inicio";
+            this.PlazoI.Name = "PlazoI";
+            this.PlazoI.ReadOnly = true;
+            // 
+            // PlazoF
+            // 
+            this.PlazoF.HeaderText = "Plazo Final";
+            this.PlazoF.Name = "PlazoF";
+            this.PlazoF.ReadOnly = true;
+            // 
+            // Tasa
+            // 
+            this.Tasa.HeaderText = "Tasa";
+            this.Tasa.Name = "Tasa";
+            this.Tasa.ReadOnly = true;
+            // 
+            // txt_Tasa
+            // 
+            this.txt_Tasa.Location = new System.Drawing.Point(387, 60);
+            this.txt_Tasa.Name = "txt_Tasa";
+            this.txt_Tasa.Size = new System.Drawing.Size(113, 20);
+            this.txt_Tasa.TabIndex = 5;
+            this.txt_Tasa.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Solo_Decimal);
+            // 
+            // txt_PlazoF
+            // 
+            this.txt_PlazoF.Location = new System.Drawing.Point(230, 60);
+            this.txt_PlazoF.Name = "txt_PlazoF";
+            this.txt_PlazoF.Size = new System.Drawing.Size(113, 20);
+            this.txt_PlazoF.TabIndex = 4;
+            this.txt_PlazoF.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Solo_Num);
+            // 
+            // txt_PlazoI
+            // 
+            this.txt_PlazoI.Location = new System.Drawing.Point(73, 60);
+            this.txt_PlazoI.Name = "txt_PlazoI";
+            this.txt_PlazoI.Size = new System.Drawing.Size(113, 20);
+            this.txt_PlazoI.TabIndex = 3;
+            this.txt_PlazoI.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Solo_Num);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(384, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tasa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(227, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Plazo Final";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(70, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Plazo Inicial";
+            // 
+            // ErrorP
+            // 
+            this.ErrorP.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Fondo);
+            this.Name = "Form1";
+            this.Text = "Plazos de Certificados de Inversión";
+            this.Fondo.ResumeLayout(false);
+            this.Fondo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Datos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorP)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Fondo;
+        private System.Windows.Forms.Button btn_Agregar;
+        private System.Windows.Forms.DataGridView Datos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PlazoI;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PlazoF;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tasa;
+        private System.Windows.Forms.TextBox txt_Tasa;
+        private System.Windows.Forms.TextBox txt_PlazoF;
+        private System.Windows.Forms.TextBox txt_PlazoI;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ErrorProvider ErrorP;
+    }
+}
+
---
+++ Trabajo/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabajo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Solo_Decimal(object sender, KeyPressEventArgs e)
+        {
+            ErrorP.SetError((Control)sender, "");
+            TextBox t = sender as TextBox;
+            if ((e.KeyChar >= 32 && e.KeyChar <= 45) || e.KeyChar == 47 ||(e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                ErrorP.SetError((Control)sender, "Solo se permite números enteros");
+                e.Handled = true;
+            }
+        }
+
+        private void Solo_Num(object sender, KeyPressEventArgs e)
+        {
+            ErrorP.SetError((Control)sender, "");
+            TextBox t = sender as TextBox;
+            if((e.KeyChar >= 32 && e.KeyChar <= 47)||(e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                ErrorP.SetError((Control)sender, "Solo se permite números naturales");
+                e.Handled = true;
+            }
+        }
+
+        private void Vacio (object sender, EventArgs e)
+        {
+            TextBox t = sender as TextBox;
+            if(t.Text == "")
+            {
+                ErrorP.SetError(t, "El campo no puede estar vació");
+            }
+        }
+
+        private void btn_Agregar_Click(object sender, EventArgs e)
+        {
+            bool correcto = true;
+            if(txt_PlazoI.Text == "")
+            {
+                ErrorP.SetError(txt_PlazoI, "Debe ingresar un valor");
+                correcto = false;
+            }
+
+            if (txt_PlazoF.Text == "")
+            {
+                ErrorP.SetError(txt_PlazoF, "Debe ingresar un valor");
+                correcto = false;
+            }
+
+            if (txt_Tasa.Text == "")
+            {
+                ErrorP.SetError(txt_Tasa, "Debe ingresar un valor");
+                correcto = false;
+            }
+
+
+            if (correcto)
+            {
+                int rangoI = int.Parse(txt_PlazoI.Text);
+                int rangoF = int.Parse(txt_PlazoF.Text);
+                double tasa = double.Parse(txt_Tasa.Text);
+
+                int i = Validar(rangoI, rangoF, tasa);
+                if (i == 0)
+                {
+                    ActualizarGrid();
+                }
+                
+                txt_PlazoI.Text = "";
+                txt_PlazoF.Text = "";
+                txt_Tasa.Text = "";
+
+            }
+
+        }
+
+        int Validar(int rangoI, int rangoF, double tasa)
+        {
+            if (rangoF <= rangoI || rangoI < 0)
+            {
+                MessageBox.Show("Rango no valido","Error en el Plazo");
+                return -1;
+            }
+
+            Plazos p;
+            p.RangoI = rangoI;
+            p.RangoF = rangoF;
+            p.Tasa = tasa;
+
+            //Caso 0: si no hay datos se agrega normalmente
+            if(Valores.Count == 0) { 
+                Valores.Add(p);
+                return 0;
+            }
+
+            int pos = BusquedaBinaria(rangoI);
+
+            //Caso 1: Cuando el plazo debe ser agregado al principio de la lista.
+            if (pos == -1 && rangoF < Valores[0].RangoI && tasa < Valores[0].Tasa)
+            {
+                Valores.Insert(0, p);
+                return 0;
+            }
+
+            //Caso 2: Cuando el plazo debe ser agregado al final de la lista.
+            if (pos == Valores.Count() - 1 && rangoI > Valores[pos].RangoF && tasa > Valores[pos].Tasa)
+            {
+                Valores.Add(p);
+                return 0;
+            }
+
+            //Caso 3: Cuando el plazo debe ser agregado en alguna posición intermedia de la lista.
+            if (pos > -1 && pos < Valores.Count() - 1)
+            {
+                if (rangoI > Valores[pos].RangoF && rangoF < Valores[pos + 1].RangoI && tasa > Valores[pos].Tasa && tasa < Valores[pos + 1].Tasa)
+                {
+                    Valores.Insert( pos + 1, p);
+                    return 0;
+                }
+            }
+
+            MessageBox.Show("Plazo Incorrecto","Error en el Plazo");
+            return -1;
+        }
+
+        /*
+         * Esta es una busqueda binaria para el termino más grande que sea
+         * menor que el valor buscado (Rango Inicial del nuevo plazo). La búsqueda binaria
+         * no es completa, cuando los valores son muy cercanos se hace una búsqueda
+         * secuencial para asegurar los valores
+         */
+        int BusquedaBinaria(int buscado)
+        {
+            int inicio = 0, fin = Valores.Count();
+
+            while (inicio + 3 < fin)
+            {
+                int mitad = (inicio + fin) / 2;
+                if (Valores[mitad].RangoF <= buscado) inicio = mitad;
+                else fin = mitad - 1;
+            }
+
+            while (inicio < fin)
+            {
+                if (Valores[inicio].RangoF <= buscado)
+                {
+                    inicio++;
+                }
+                else break;
+            }
+            return inicio - 1;
+        }
+
+        struct Plazos
+        {
+            public int RangoI;
+            public int RangoF;
+            public double Tasa;
+        }
+
+        List<Plazos> Valores = new List<Plazos>();
+
+        /*Forma de Actualizar los datos de la tabla, agregando cada valor que hay en la lista*/
+        private void ActualizarGrid()
+        {
+            Datos.Rows.Clear();
+            Valores.ForEach(x => { 
+                Datos.Rows.Add(x.RangoI,x.RangoF,x.Tasa.ToString("0.00##"));
+            });
+        }
+    }
+}
